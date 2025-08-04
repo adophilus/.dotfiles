d2babf466c5b202b08ff79a6f0c9ec4c198e1f41
Adophilus <uchenna19of@gmail.com>
2025-08-04 21:28:54 +0100

    chore: updated kitty config

--- .config/nvim/lua/community.lua
+++ .config/nvim/lua/community.lua
@@ -7,14 +7,19 @@ if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
+  { import = "astrocommunity.pack.cpp" },
+  { import = "astrocommunity.pack.just" },
   { import = "astrocommunity.pack.lua" },
+  { import = "astrocommunity.pack.nix" },
+  { import = "astrocommunity.pack.php" },
   { import = "astrocommunity.pack.purescript" },
   -- { import = "astrocommunity.pack.typescript-deno" },
   -- { import = "astrocommunity.pack.typescript-all-in-one" },
   -- import/override with your plugins folder
+  { import = "astrocommunity.recipes.ai" },
   { import = "astrocommunity.recipes.vscode" },
-  { import = "astrocommunity.recipe.neovide" },
-  { import = "astrocommunity.recipe.auto-session-restore" },
-  { import = "astrocommunity.recipe.astrolsp-auto-signature-help" },
-  { import = "astrocommunity.recipe.telescope-lsp-mappings" },
+  { import = "astrocommunity.recipes.neovide" },
+  { import = "astrocommunity.recipes.auto-session-restore" },
+  { import = "astrocommunity.recipes.astrolsp-auto-signature-help" },
+  { import = "astrocommunity.recipes.telescope-lsp-mappings" },
 }

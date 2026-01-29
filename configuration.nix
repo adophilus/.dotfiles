45d7a64dd8b795f0cbf14e585695141946b345ef
Adophilus <uchenna19of@gmail.com>
2026-01-29 17:14:44 +0100

    chore: updated flake

--- configuration.nix
+++ configuration.nix
@@ -11,16 +11,28 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  services.power-profiles-daemon.enable = false;
-
-  services.thermald.enable = true;
-
+  # Power management
+  services.system76-scheduler.settings.cfsProfiles.enable =
+    true; # Better scheduling for CPU cycles - thanks System76!!!
+  services.thermald.enable =
+    true; # Enable thermald, the temperature management daemon. (only necessary if on Intel CPUs)
+  services.power-profiles-daemon.enable =
+    false; # Disable GNOMEs power management
   services.tlp = {
+    enable = true; # Enable TLP (better than gnomes internal power manager)
     settings = {
       CPU_BOOST_ON_AC = 1;
       CPU_BOOST_ON_BAT = 0;
+      CPU_HWP_DYN_BOOST_ON_AC = 1;
+      CPU_HWP_DYN_BOOST_ON_BAT = 0;
       CPU_SCALING_GOVERNOR_ON_AC = "performance";
       CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+      PLATFORM_PROFILE_ON_AC = "performance";
+      PLATFORM_PROFILE_ON_BAT = "low-power";
+      START_CHARGE_THRESH_BAT0 = 75;
+      STOP_CHARGE_THRESH_BAT0 = 81;
     };
   };
 
@@ -190,6 +202,8 @@
   };
 
   # programs.command-not-found.enable = false;
+
+  hardware.intel-gpu-tools.enable = true;
 
   hardware.bluetooth = {
     enable = true;

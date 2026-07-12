75efa880d2bf3fa9fc6c0c3257c3e78aa8bd1b5a
Adophilus <uchenna19of@gmail.com>
2026-07-12 14:05:57 +0100

    chore: added copy config for ~/.agents folder

--- config/programs/opencode/default.nix
+++ config/programs/opencode/default.nix
@@ -21,4 +21,9 @@
     run ${pkgs.rsync}/bin/rsync --archive ${../../../.config/opencode}/ $HOME/.config/opencode/
     run chmod --recursive u+w $HOME/.config/opencode
   '';
+
+  home.activation.copyAgentsConfig = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    run ${pkgs.rsync}/bin/rsync --archive ${../../../.config/.agents}/ $HOME/.agents/
+    run chmod --recursive u+w $HOME/.agents
+  '';
 }

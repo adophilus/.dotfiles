2333e949460ed9eeba1e81e0f5f7d0a6a6ad59b7
Adophilus <uchenna19of@gmail.com>
2026-07-13 14:48:19 +0100

    chore: updated the opencode .config contents

--- config/programs/opencode/default.nix
+++ config/programs/opencode/default.nix
@@ -20,6 +20,10 @@
     run chmod --recursive u+w $HOME/.config/opencode
   '';
 
+  home.file.".config/opencode/AGENTS.md".source = ../../../.config/opencode/AGENTS.md;
+  home.file.".config/opencode/oh-my-opencode-slim.jsonc".source = ../../../.config/opencode/oh-my-opencode-slim.jsonc;
+  home.file.".config/opencode/opencode.jsonc".source = ../../../.config/opencode/opencode.jsonc;
+
   home.activation.copyAgentsConfig = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
     run ${pkgs.rsync}/bin/rsync --delete --archive ${../../../.config/.agents}/ $HOME/.agents/
     run chmod --recursive u+w $HOME/.agents

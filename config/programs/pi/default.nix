002304f36b971ce23f1a76d6afc1095ccd5f2878
Adophilus <uchenna19of@gmail.com>
2026-08-26 09:26:07 +0100

    chore: added new pi plugin

--- config/programs/pi/default.nix
+++ config/programs/pi/default.nix
@@ -2,7 +2,7 @@
 
 {
   home.file.".pi/agent/settings.json".source = ../../../home/.pi/agent/settings.json;
-
+  home.file.".pi/agent/zentui.json".source = ../../../home/.pi/agent/zentui.json;
   home.file.".pi/agent/AGENTS.md".source = ../../../home/.pi/agent/AGENTS.md;
 
   home.file.".pi/agent/agents" = {

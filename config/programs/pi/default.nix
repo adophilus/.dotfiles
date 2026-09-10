0176a36d7072a2161a2cee20c65a31c909e75a24
Adophilus <uchenna19of@gmail.com>
2026-09-10 10:11:47 +0100

    chore: added pi pop config

--- config/programs/pi/default.nix
+++ config/programs/pi/default.nix
@@ -1,6 +1,7 @@
 { ... }:
 
 {
+  home.file.".pi/pi-pop.json".source = ../../../home/.pi/pi-pop.json;
   home.file.".pi/agent/settings.json".source = ../../../home/.pi/agent/settings.json;
   home.file.".pi/agent/zentui.json".source = ../../../home/.pi/agent/zentui.json;
   home.file.".pi/agent/AGENTS.md".source = ../../../home/.pi/agent/AGENTS.md;
